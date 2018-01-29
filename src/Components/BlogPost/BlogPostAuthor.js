3ae53b2f904d7a287f837ca1f2eb25f7a53ff7bb
Alexander Pepper <alexander.pepper@infopark.de>
2018-01-29 11:02:51 +0000

    BlogPost: Do not render an author image placeholder, if missing.
    Instead only render the name and the description.

--- src/Components/BlogPost/BlogPostAuthor.js
+++ src/Components/BlogPost/BlogPostAuthor.js
@@ -9,21 +9,43 @@ function BlogPostAuthor({ author }) {
       <div className="container">
         <hr />
         <div className="row">
-          <div className="col-md-1 col-sm-4 col-xs-4">
-            <Scrivito.LinkTag to={ author }>
-              <AuthorImage image={ author.get('image') }/>
-            </Scrivito.LinkTag>
-          </div>
-          <div className="col-md-11 col-sm-8 col-xs-8">
-            <Scrivito.LinkTag to={ author }>
-              <strong className="text-greymiddle">{ author.get('title') }</strong>
-            </Scrivito.LinkTag>
-            <p>{ author.get('description') }</p>
-          </div>
+          <AuthorImageAndDescription author={ author } />
         </div>
       </div>
     </section>
   );
 }
+
+const AuthorImageAndDescription = Scrivito.connect(({ author }) => {
+  if (!author.get('image')) {
+    return (
+      <div className="col-md-12 col-sm-12 col-xs-12">
+        <AuthorDescription author={ author } />
+      </div>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <div className="col-md-1 col-sm-4 col-xs-4">
+        <Scrivito.LinkTag to={ author }>
+          <AuthorImage image={ author.get('image') }/>
+        </Scrivito.LinkTag>
+      </div>
+      <div className="col-md-11 col-sm-8 col-xs-8">
+        <AuthorDescription author={ author } />
+      </div>
+    </React.Fragment>
+  );
+});
+
+const AuthorDescription = Scrivito.connect(({ author }) =>
+  <React.Fragment>
+    <Scrivito.LinkTag to={ author }>
+      <strong className="text-greymiddle">{ author.get('title') }</strong>
+    </Scrivito.LinkTag>
+    <p>{ author.get('description') }</p>
+  </React.Fragment>
+);
 
 export default Scrivito.connect(BlogPostAuthor);

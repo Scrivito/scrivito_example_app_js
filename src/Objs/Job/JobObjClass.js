3dd12e557f981f28448a5f0f5bfab7599a41ac2c
Alexander Schrot <alexander.schrot@infopark.de>
2018-01-30 14:26:05 +0100

    add extra fields to job obj class for schema.org

--- src/Objs/Job/JobObjClass.js
+++ src/Objs/Job/JobObjClass.js
@@ -7,6 +7,33 @@ const Job = Scrivito.provideObjClass('Job', {
     image: 'reference',
     location: 'string',
     title: 'string',
+
+    datePosted: 'date',
+    validThrough: 'date',
+    description: 'html',
+
+    hiringOrganizationName: 'string',
+    hiringOrganizationWebsite: 'string',
+
+    locationStreetAddress: 'string',
+    locationLocality: 'string',
+    locationRegion: 'string',
+    locationPostalCode: 'string',
+    locationCountry: 'string',
+
+    employmentType: ['multienum', {
+      values: [
+        'FULL_TIME',
+        'PART_TIME',
+        'CONTRACTOR',
+        'TEMPORARY',
+        'INTERN',
+        'VOLUNTEER',
+        'PER_DIEM',
+        'OTHER',
+      ],
+    }],
+
     ...metaDataAttributes,
   },
 });

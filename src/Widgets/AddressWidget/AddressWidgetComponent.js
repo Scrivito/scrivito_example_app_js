e9f6664f6c7d99dfe54ddc2588d1385ae7c02398
Alexander Schrot <alexander.schrot@infopark.de>
2018-01-30 16:59:19 +0100

    use static fields in address widget component

--- src/Widgets/AddressWidget/AddressWidgetComponent.js
+++ src/Widgets/AddressWidget/AddressWidgetComponent.js
@@ -1,52 +1,17 @@
-import AddressListItemWidget from 'Widgets/AddressListItemWidget/AddressListItemWidgetClass';
-import placeholderCss from 'utils/placeholderCss';
-
-const AddRow = Scrivito.connect(({ widget }) => {
-  if (!Scrivito.isInPlaceEditingActive()) { return null; }
-
-  return (
-    <tr>
-      <td className="text-center" colSpan="2">
-        <a
-          href='#'
-          style={ placeholderCss }
-          onClick={
-            e => {
-              e.preventDefault();
-              e.stopPropagation();
-
-              const newListItems =
-                [...widget.get('listItems'), new AddressListItemWidget({})];
-              widget.update({ listItems: newListItems });
-            }
-          }
-        >
-          Click to add a row
-        </a>
-      </td>
-    </tr>
-  );
-});
+import InPlaceEditingPlaceholder from 'Components/InPlaceEditingPlaceholder';
 
 Scrivito.provideComponent('AddressWidget', ({ widget }) => {
   return (
     <div>
       { widget.get('showLogo') !== 'no' && <Logo /> }
       <address>
-        <Scrivito.ContentTag content={ widget } attribute="address" />
-        <table>
-          <tbody>
-            {
-              widget.get('listItems').map((listItem, i) =>
-                <tr key={ i }>
-                  <Scrivito.ContentTag content={ listItem } attribute="key" tag="td" />
-                  <Scrivito.ContentTag content={ listItem } attribute="value" tag="td" />
-                </tr>
-              )
-            }
-            <AddRow widget={ widget } />
-          </tbody>
-        </table>
+        <Address addressWidget={ widget } />
+        <Table
+            phone={ widget.get('phone') }
+            mobile={ widget.get('mobile') }
+            fax={ widget.get('fax') }
+            eMail={ widget.get('eMail') }
+          />
       </address>
       { widget.get('showBorderBottom') === 'yes' && <div className="border-bottom" /> }
     </div>
@@ -66,3 +31,84 @@ const Logo = Scrivito.connect(() => {
     </Scrivito.LinkTag>
   );
 });
+
+const Address = Scrivito.connect(({ addressWidget }) => {
+  const localityRegionPostalCode = [
+    addressWidget.get('locationLocality'),
+    addressWidget.get('locationRegion'),
+    addressWidget.get('locationPostalCode'),
+  ].filter(n => n).join(' ');
+
+  const lines = [
+    addressWidget.get('locationName'),
+    addressWidget.get('locationStreetAddress'),
+    localityRegionPostalCode,
+    addressWidget.get('locationCountry'),
+  ].filter(n => n);
+
+
+  if (!lines.length) {
+    return (
+      <InPlaceEditingPlaceholder>
+        Provide the location in the address widget properties.
+      </InPlaceEditingPlaceholder>
+    );
+  }
+
+  const breakedLines = lines.reduce((array, value) => {
+    array.push(value);
+    array.push(<br key={ value } />);
+    return array;
+  }, []);
+
+  return <p>{ breakedLines }</p>;
+});
+
+function Table(values) {
+  const columns = Object.keys(values).map(name => {
+    return <TableColumn key={ name } name={ name } value={ values[name] } />;
+  });
+
+  if (columns.length === 0) {
+    return null;
+  }
+
+  return (
+    <table>
+      <tbody>
+        { columns }
+      </tbody>
+    </table>
+  );
+}
+
+function TableColumn({ name, value }) {
+  const localization = {
+    phone: 'Phone',
+    mobile: 'Mobile',
+    fax: 'Fax',
+    eMail: 'E-Mail',
+  };
+
+  const linkPrefixes = {
+    phone: 'tel',
+    mobile: 'tel',
+    fax: 'fax',
+    eMail: 'mailto',
+  };
+
+  const prefix = linkPrefixes[name];
+
+  if (!value) {
+    return null;
+  }
+
+  const href = prefix ? `${prefix}:${value}` : value;
+
+  return (
+    <tr>
+      <td>{ `${localization[name]}: ` }</td>
+      <td><a href={ href }>{ value }</a></td>
+    </tr>
+  );
+}

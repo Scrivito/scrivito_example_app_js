a5e783dfb8983d82062c775144f4886c93acfcf7
Alexander Pepper <alexander.pepper@infopark.de>
2018-02-07 14:38:10 +0000

    Added workaround missing window.React.
    See https://github.com/infopark/rails_connector/issues/3829 for the bugreport.

--- src/scrivito_extensions.js
+++ src/scrivito_extensions.js
@@ -2,3 +2,8 @@ import 'Objs/';
 import 'Widgets/';
 import 'config/';
 import 'Components/ScrivitoExtensions/';
+
+// TODO: Remove once https://github.com/infopark/rails_connector/issues/3829 is resolved.
+import * as React from 'react';
+
+window.React = React;

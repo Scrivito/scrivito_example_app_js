dc804c61fa4c5dcc7ae2a25e5b18b9b1e37f4d75
Alexander Pepper <alexander.pepper@infopark.de>
2018-02-08 11:43:44 +0100

    Fix order of absolut and relative imports.
    Inlined "reactPolyfills", otherwise the order is no longer given.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import './reactPolyfills';
+// Needed polyfills for React 16 for older browsers
+// See https://reactjs.org/blog/2017/09/26/react-v16.0.html#javascript-environment-requirements
+import 'core-js/modules/es6.map';
+import 'core-js/modules/es6.set';
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './Objs';
--- src/scrivito_extensions.js
+++ src/scrivito_extensions.js
@@ -1,9 +1,8 @@
+import * as React from 'react';
 import './Objs';
 import './Widgets';
 import './config';
 import './Components/ScrivitoExtensions';
 
 // TODO: Remove once https://github.com/infopark/rails_connector/issues/3829 is resolved.
-import * as React from 'react';
-
 window.React = React;

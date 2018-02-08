26c7a1e095007b2944ca4fef82c0fc5add3fa078
Alexander Pepper <alexander.pepper@infopark.de>
2018-02-08 13:45:37 +0100

    prerender - ErrorBoundary code status is now 500.

--- src/Components/ErrorBoundary.js
+++ src/Components/ErrorBoundary.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Helmet from 'react-helmet';
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
@@ -17,26 +18,29 @@ class ErrorBoundary extends React.Component {
     }
 
     return (
-      <section
-        className="bg-dark-image full-height"
-        style={ { background: 'no-repeat center / cover', backgroundImage } }
-      >
-        <div className="container">
-          <div className="text-center"><h1 className="hero-bold">Sorry</h1></div>
-          <div className="text-center">
-            <h2 className="hero-small">Something went wrong.</h2>
+      <React.Fragment>
+        <section
+          className="bg-dark-image full-height"
+          style={ { background: 'no-repeat center / cover', backgroundImage } }
+        >
+          <div className="container">
+            <div className="text-center"><h1 className="hero-bold">Sorry</h1></div>
+            <div className="text-center">
+              <h2 className="hero-small">Something went wrong.</h2>
+            </div>
+            <div className="text-center">
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={ () => window.location.reload(true) }
+              >
+                Please try again<i className="fa fa-angle-right fa-4" aria-hidden="true" />
+              </button>
+            </div>
           </div>
-          <div className="text-center">
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={ () => window.location.reload(true) }
-            >
-              Please try again<i className="fa fa-angle-right fa-4" aria-hidden="true" />
-            </button>
-          </div>
-        </div>
-      </section>
+        </section>
+        <Helmet meta={ [{ name: 'prerender-status-code', content: '500' }] } />
+      </React.Fragment>
     );
   }
 }

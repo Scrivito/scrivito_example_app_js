df47b4b512f7145f7c6e33d11ed3dc156a046a80
Alexander Schrot <alexander.schrot@infopark.de>
2018-02-09 14:25:34 +0100

    add description and image to schema.org of an author component

--- src/Components/SchemaDotOrg/dataFromAuthor.js
+++ src/Components/SchemaDotOrg/dataFromAuthor.js
@@ -1,8 +1,12 @@
+import urlFromBinary from '../../utils/urlFromBinary';
+
 function dataFromAuthor(author) {
   return {
     '@context': 'http://schema.org',
     '@type': 'Person',
     name: author.get('title'),
+    description: author.get('description'),
+    image: urlFromBinary(author.get('image')),
   };
 }
 

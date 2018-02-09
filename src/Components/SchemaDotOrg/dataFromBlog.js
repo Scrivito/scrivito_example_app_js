8aa77db09ddf867d3f811f2bd48950cabbe588f7
Alexander Schrot <alexander.schrot@infopark.de>
2018-02-09 14:28:48 +0100

    add image attribute to schema.org of blog component

--- src/Components/SchemaDotOrg/dataFromBlog.js
+++ src/Components/SchemaDotOrg/dataFromBlog.js
@@ -1,8 +1,11 @@
+import urlFromBinary from '../../utils/urlFromBinary';
+
 function dataFromBlog(blog) {
   return {
     '@context': 'http://schema.org',
     '@type': 'Blog',
     headline: blog.get('title'),
+    image: urlFromBinary(blog.get('navigationBackgroundImage')),
   };
 }
 

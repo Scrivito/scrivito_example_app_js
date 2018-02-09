23f31c9eb7c09d47891edb93f5f94985d6d49d96
Alexander Schrot <alexander.schrot@infopark.de>
2018-02-09 11:31:06 +0100

    use dataFromAuthor in dataFromBlogPost

--- src/Components/SchemaDotOrg/dataFromBlogPost.js
+++ src/Components/SchemaDotOrg/dataFromBlogPost.js
@@ -1,31 +1,26 @@
 import truncate from 'lodash/truncate';
+import dataFromAuthor from './dataFromAuthor';
 import formatDate from '../../utils/formatDate';
 import urlFromBinary from '../../utils/urlFromBinary';
 import { textExtractFromWidgetlist } from '../../utils/textExtract';
 
 function dataFromBlogPost(blogPost) {
+  let author;
   const body = textExtractFromWidgetlist(blogPost.get('body'));
+
+  if (blogPost.get('author')) {
+    author = dataFromAuthor(blogPost.get('author'));
+  }
 
   return {
     '@context': 'http://schema.org',
     '@type': 'BlogPosting',
-    author: personFromBlogPost(blogPost.get('author')),
+    author,
     datePublished: formatDate(blogPost.get('publishedAt'), 'yyyy-mm-dd'),
     description: truncate(body, { length: 300 }),
     headline: blogPost.get('title'),
     image: urlFromBinary(blogPost.get('titleImage')),
     keywords: blogPost.get('tags').join(', '),
-  };
-}
-
-function personFromBlogPost(author) {
-  if (!author) {
-    return;
-  }
-
-  return {
-    '@type': 'Person',
-    name: author.get('title'),
   };
 }
 

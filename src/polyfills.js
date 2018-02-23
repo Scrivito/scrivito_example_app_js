98f71ac64678b46b3df1a85b52c0d423dafbdf0b
Alexander Schrot <alexander.schrot@infopark.de>
2018-02-23 10:50:19 +0100

    add needed polyfills by hand

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,17 +1,8 @@
-import 'core-js/modules/es6.array.find';
-import 'core-js/modules/es6.array.from';
-import 'core-js/modules/es6.array.sort';
-import 'core-js/modules/es6.function.name';
+// Needed polyfills for React 16 for older browsers  +import './polyfills';
+// See https://reactjs.org/blog/2017/09/26/react-v16.0.html#javascript-environment-requirements
 import 'core-js/modules/es6.map';
-import 'core-js/modules/es6.object.assign';
-import 'core-js/modules/es6.object.set-prototype-of';
-import 'core-js/modules/es6.regexp.replace';
-import 'core-js/modules/es6.regexp.search';
-import 'core-js/modules/es6.regexp.split';
 import 'core-js/modules/es6.set';
-import 'core-js/modules/es6.string.includes';
-import 'core-js/modules/es6.string.starts-with';
+
+// Needed polyfill to support an iteratable
+// ObjSearch (https://www.scrivito.com/objsearch-aaa67c3157464d01)
 import 'core-js/modules/es6.symbol';
-import 'core-js/modules/es7.array.includes';
-import 'core-js/modules/es7.object.entries';
-import 'core-js/modules/web.dom.iterable';

f5db5d31a8a96aae3eed54f71d33b8c86c62e551
Alexander Pepper <alexander.pepper@infopark.de>
2018-03-28 10:20:27 +0000

    Inlined getTrackingId in GoogleAnalytics.

--- src/Components/GoogleAnalytics.js
+++ src/Components/GoogleAnalytics.js
@@ -10,7 +10,11 @@ class GoogleAnalytics extends React.Component {
   }
 
   componentDidMount() {
-    Scrivito.load(() => this.getTrackingId()).then(trackingId => {
+    Scrivito.load(() => {
+      const rootPage = Scrivito.Obj.root();
+      if (!rootPage) { return; }
+      return rootPage.get('googleAnalyticsTrackingId');
+    }).then(trackingId => {
       if (trackingId) {
         window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
         window.ga('create', trackingId, 'auto');
@@ -34,16 +38,6 @@ class GoogleAnalytics extends React.Component {
         <script async src='https://www.google-analytics.com/analytics.js'></script>
       </Helmet>
     );
-  }
-
-  getTrackingId() {
-    const rootPage = Scrivito.Obj.root();
-
-    if (!rootPage) {
-      return;
-    }
-
-    return rootPage.get('googleAnalyticsTrackingId');
   }
 }
 

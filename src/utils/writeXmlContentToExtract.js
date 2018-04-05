df5ea031b7c4e3c5172fa3051aa24224a8eb8667
Alexander Pepper <alexander.pepper@infopark.de>
2018-04-05 12:22:23 +0200

    sitemap.js: Inlined all helper functions => only one file.

--- src/utils/writeXmlContentToExtract.js
+++
@@ -1,6 +0,0 @@
-function writeXmlContentToExtract(content) {
-  const metaTag = document.getElementsByName('scrivito-content-to-extract')[0];
-  metaTag.setAttribute('content', `data:text/xml;base64,${btoa(content)}`);
-}
-
-export default writeXmlContentToExtract;

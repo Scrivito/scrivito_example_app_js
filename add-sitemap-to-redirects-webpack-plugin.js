a4bf2f382003850ab15fb6bc8ca524146ec0bf2d
Alexander Pepper <alexander.pepper@infopark.de>
2018-04-05 12:18:06 +0200

    AddSitemapToRedirectsWebpackPlugin: Added a comment

--- add-sitemap-to-redirects-webpack-plugin.js
+++ add-sitemap-to-redirects-webpack-plugin.js
@@ -25,8 +25,12 @@ function emit(compilation, callback) {
 
 function addSitemapTRedirects(tenant, url, previousRedirects) {
   return `
+##
+# Sitemap redirect rules
+##
 /_sitemaphtml /_sitemap.html 200
 /sitemap.xml https://api.scrivito.com/tenants/${tenant}/extract_content?src_url=${encodeURIComponent(url)}%2F_sitemaphtml%3F_escaped_fragment_%3D0 302
+
 ${previousRedirects}`;
 }
 

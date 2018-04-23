7069cdda6b5ad88fb61e916be0d80b1e73ecd64c
Alexander Pepper <alexander.pepper@infopark.de>
2018-04-23 15:15:53 +0200

    Tell prerenderer, when Scrivito is done loading.
    See https://prerender.io/documentation/best-practices for details.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import './polyfills';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as Scrivito from 'scrivito';
 import './Objs';
 import './Widgets';
 import App from './App';
 import './config';
 
+window.prerenderReady = false;
 ReactDOM.render(<App />, document.getElementById('application'));
+Scrivito.finishLoading().then(() => { window.prerenderReady = true; });

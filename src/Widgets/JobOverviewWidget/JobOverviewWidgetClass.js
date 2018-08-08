77279799d91bf53dc3e49b8b08805a893c5f28f5
Kamila Skonieczna <kamila.skonieczna@infopark.de>
2018-08-08 15:10:43 +0200

    Changed name of old attribute into current one, changed title and description of widget

--- src/Widgets/JobOverviewWidget/JobOverviewWidgetClass.js
+++ src/Widgets/JobOverviewWidget/JobOverviewWidgetClass.js
@@ -2,7 +2,7 @@ import * as Scrivito from "scrivito";
 
 const JobOverviewWidget = Scrivito.provideWidgetClass("JobOverviewWidget", {
   attributes: {
-    location: "string",
+    locationLocality: "string",
   },
 });
 

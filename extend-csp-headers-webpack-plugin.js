77e6340c435f9c36ae0ba1f7b65aab6bb1a9a7c0
Kamila Skonieczna <kamila.skonieczna@infopark.de>
2018-08-22 14:41:09 +0200

    Created specific function to create headers file

--- extend-csp-headers-webpack-plugin.js
+++ extend-csp-headers-webpack-plugin.js
@@ -7,49 +7,55 @@ function ExtendCspHeadersWebpackPlugin() {
 }
 
 function emit(compilation, callback) {
-  const headers = compilation.assets._headers.source().toString();
-  const headersCsp = compilation.assets._headers_csp.source().toString();
-  const headersCspScriptSrc = compilation.assets._headers_csp_script_src
+  const scriptSrc = generateScriptSrc(compilation.assets);
+  const csp = generateCsp(compilation.assets, scriptSrc);
+  extendHeaders(compilation.assets, csp);
+  callback();
+}
+
+function generateScriptSrc(assets) {
+  const headersCspScriptSrc = assets._headers_csp_script_src
     .source()
     .toString();
-  const CSP_PLACEHOLDER = "Content-Security-Policy-PLACEHOLDER;";
+  delete assets._headers_csp_script_src;
+  const scriptSrcArray = splitByLine(headersCspScriptSrc);
+  scriptSrcArray.unshift("script-src");
+  return `${scriptSrcArray.join(" ")};`;
+}
+
+function generateCsp(assets, scriptSrc) {
+  const headersCsp = assets._headers_csp.source().toString();
+  delete assets._headers_csp;
   const SCRIPT_SRC_PLACEHOLDER = "script-src-PLACEHOLDER;";
-
-  if (!headers.includes(CSP_PLACEHOLDER)) {
-    throw new Error(
-      `_headers doesn't contain '${CSP_PLACEHOLDER}'. Please add the placeholder.`
-    );
-  }
-
   if (!headersCsp.includes(SCRIPT_SRC_PLACEHOLDER)) {
     throw new Error(
       `_headers_csp doesn't contain '${SCRIPT_SRC_PLACEHOLDER}'. Please add the placeholder.`
     );
   }
-
-  const scriptSrcArray = splitByLine(headersCspScriptSrc);
-  scriptSrcArray.unshift("script-src");
-  const scriptSrc = `${scriptSrcArray.join(" ")};`;
-
   const modifiedCspHeader = headersCsp.replace(
     SCRIPT_SRC_PLACEHOLDER,
     scriptSrc
   );
   const cspArray = splitByLine(modifiedCspHeader);
   cspArray.unshift("Content-Security-Policy:");
-  const cspLine = cspArray.join(" ");
+  return cspArray.join(" ");
+}
 
-  const modifiedHeaders = headers.replace(CSP_PLACEHOLDER, cspLine);
+function extendHeaders(assets, csp) {
+  const headers = assets._headers.source().toString();
+  const CSP_PLACEHOLDER = "Content-Security-Policy-PLACEHOLDER;";
+  if (!headers.includes(CSP_PLACEHOLDER)) {
+    throw new Error(
+      `_headers doesn't contain '${CSP_PLACEHOLDER}'. Please add the placeholder.`
+    );
+  }
 
-  compilation.assets._headers = {
+  const modifiedHeaders = headers.replace(CSP_PLACEHOLDER, csp);
+
+  assets._headers = {
     source: () => modifiedHeaders,
     size: () => modifiedHeaders.length,
   };
-
-  delete compilation.assets._headers_csp;
-  delete compilation.assets._headers_csp_script_src;
-
-  callback();
 }
 
 function splitByLine(string) {

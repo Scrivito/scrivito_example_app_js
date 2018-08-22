18d9001ef79f5c03bc9b2977a5afd833b840e7c0
Kamila Skonieczna <kamila.skonieczna@infopark.de>
2018-08-22 12:24:40 +0200

    Rewrote extend-csp-headers-webpack-plugin

--- extend-csp-headers-webpack-plugin.js
+++ extend-csp-headers-webpack-plugin.js
@@ -1,5 +1,3 @@
-const fs = require("fs");
-
 function ExtendCspHeadersWebpackPlugin() {
   return {
     apply: compiler => {
@@ -9,26 +7,54 @@ function ExtendCspHeadersWebpackPlugin() {
 }
 
 function emit(compilation, callback) {
-  const cspConfig = fs.readFileSync("./src/_csp", "utf8");
-  const cspTitle = "Content-Security-Policy:";
+  const headers = compilation.assets._headers.source().toString();
+  const headersCsp = compilation.assets._headers_csp.source().toString();
+  const headersCspScriptSrc = compilation.assets._headers_csp_script_src
+    .source()
+    .toString();
 
-  if (cspConfig) {
-    let domains = "";
-    const cspWhitelist = fs.readFileSync("./src/_csp_whitelist", "utf8");
-    if (cspWhitelist) {
-      domains = cspWhitelist.replace(/\n/g, " ");
-    }
-    const cspWarnings = cspConfig
-      .replace("{{domains}}", domains)
-      .replace(/\n/g, " ");
-    const cspHeader = `${cspTitle} ${cspWarnings}`;
-
-    compilation.assets._headers = {
-      source: () => cspHeader,
-      size: () => cspHeader.length,
-    };
-    callback();
+  if (!headers.includes("Content-Security-Policy-PLACEHOLDER;")) {
+    throw new Error(
+      "_headers doesn't contain 'Content-Security-Policy-PLACEHOLDER;'. Please add the placeholder."
+    );
   }
+
+  if (!headersCsp.includes("script-src-PLACEHOLDER;")) {
+    throw new Error(
+      "_headers_csp doesn't contain 'script-src-PLACEHOLDER;'. Please add the placeholder."
+    );
+  }
+
+  const scriptSrcArray = splitByLine(headersCspScriptSrc);
+  scriptSrcArray.unshift("script-src");
+  const scriptSrc = `${scriptSrcArray.join(" ")};`;
+
+  const modifiedCspHeader = headersCsp.replace(
+    "script-src-PLACEHOLDER;",
+    scriptSrc
+  );
+  const cspArray = splitByLine(modifiedCspHeader);
+  cspArray.unshift("Content-Security-Policy:");
+  const cspLine = cspArray.join(" ");
+
+  const modifiedHeaders = headers.replace(
+    "Content-Security-Policy-PLACEHOLDER;",
+    cspLine
+  );
+
+  compilation.assets["_headers"] = {
+    source: () => modifiedHeaders,
+    size: () => modifiedHeaders.length,
+  };
+
+  delete compilation.assets["_headers_csp"];
+  delete compilation.assets["_headers_csp_script_src"];
+
+  callback();
+}
+
+function splitByLine(string) {
+  return string.split(/\r?\n/).filter(item => item);
 }
 
 module.exports = ExtendCspHeadersWebpackPlugin;

6fdbd9586cdb932c7f0e539f958dcf0205e66c7c
Kamila Skonieczna <kamila.skonieczna@infopark.de>
2018-09-05 15:38:58 +0200

    Added aos, changed static component into dynamic

--- src/Widgets/ImageWidget/ImageWidgetComponent.js
+++ src/Widgets/ImageWidget/ImageWidgetComponent.js
@@ -1,26 +1,47 @@
 import * as React from "react";
 import * as Scrivito from "scrivito";
+import "../../../node_modules/aos/dist/aos.css";
+import AOS from "aos";
 
-Scrivito.provideComponent("ImageWidget", ({ widget }) => {
-  let image = (
-    <Scrivito.ImageTag
-      content={widget}
-      attribute="image"
-      alt={alternativeText(widget)}
-    />
-  );
-
-  const link = widget.get("link");
-  if (link && !Scrivito.isInPlaceEditingActive()) {
-    image = <Scrivito.LinkTag to={link}>{image}</Scrivito.LinkTag>;
+class ImageWidget extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    AOS.init();
   }
 
-  if (["center", "right"].includes(widget.get("alignment"))) {
-    return <div className={`text-${widget.get("alignment")}`}>{image}</div>;
+  componentWillReceiveProps() {
+    AOS.refresh();
   }
 
-  return image;
-});
+  render() {
+    const widget = this.props.widget;
+    let image = (
+      <Scrivito.ImageTag
+        content={widget}
+        attribute="image"
+        alt={alternativeText(widget)}
+      />
+    );
+    const link = widget.get("link");
+    if (link && !Scrivito.isInPlaceEditingActive()) {
+      image = <Scrivito.LinkTag to={link}>{image}</Scrivito.LinkTag>;
+    }
+
+    if (["center", "right"].includes(widget.get("alignment"))) {
+      return <div className={`text-${widget.get("alignment")}`}>{image}</div>;
+    }
+
+    return (
+      <div
+        data-aos="fade-up"
+        data-aos-duration="2000"
+        data-aos-easing="ease-in-quad"
+      >
+        {image}
+      </div>
+    );
+  }
+}
 
 function alternativeText(widget) {
   const widgetAlternativeText = widget.get("alternativeText");
@@ -35,3 +56,5 @@ function alternativeText(widget) {
 
   return "";
 }
+
+Scrivito.provideComponent("ImageWidget", ImageWidget);

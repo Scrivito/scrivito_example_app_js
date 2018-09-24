9f85402fadfe96a7685f6b970c391347711ba2e4
Kamila Skonieczna <kamila.skonieczna@infopark.de>
2018-09-24 16:04:16 +0200

    Extended templates for widgets

--- generator-scrivito/generators/widget/templates/XWidgetClass.js
+++ generator-scrivito/generators/widget/templates/XWidgetClass.js
@@ -1,9 +1,14 @@
 import * as Scrivito from "scrivito";
+import { registerTextExtract } from "../../utils/textExtractRegistry";
 
 const <%= nameUpperWidget %> = Scrivito.provideWidgetClass("<%= nameUpperWidget %>", {
   attributes: {
     headline: "string",
   },
 });
+
+registerTextExtract("<%= nameUpperWidget %>", [
+  { attribute: "headline", type: "string" },
+]);
 
 export default <%= nameUpperWidget %>;

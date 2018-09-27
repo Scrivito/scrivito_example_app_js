e2851f70b9f8c7567a869ebd89b95d9ac5cc8479
Kamila Skonieczna <kamila.skonieczna@infopark.de>
2018-09-27 15:45:20 +0200

    Get rid of class and config for object

--- generator-scrivito/generators/obj/index.js
+++ generator-scrivito/generators/obj/index.js
@@ -1,36 +1,18 @@
 const Generator = require("yeoman-generator");
+const lodash = require("lodash");
 
 module.exports = class extends Generator {
   start() {
-    this.prompt([
-      {
-        type: "input",
-        name: "objClassName",
-        message: "Enter the name of the Obj (e.g. MyPage):",
-      },
-      {
-        type: "list",
-        name: "type",
-        message: "What do you want to generate?",
-        choices: [
-          "Generate an Obj with a react component (default)",
-          "Generate an Obj without a react component (e.g. like a download)",
-        ],
-      },
-    ]).then(answers => {
+    this.prompt({
+      type: "input",
+      name: "objClassName",
+      message: "Enter the name of the Obj (e.g. MyPage):",
+    }).then(answers => {
       const objClassName = answers.objClassName;
-
-      switch (answers.type) {
-        case "Generate an Obj with a react component (default)":
-          this._writeXObjComponent(objClassName);
-          this._writeXEditingConfig(objClassName);
-          this._writeXObjClass(objClassName);
-          break;
-        case "Generate an Obj without a react component (e.g. like a download)":
-          this._writeYEditingConfig(objClassName);
-          this._writeYObjClass(objClassName);
-          break;
-      }
+      const humanFriendlyName = lodash.startCase(objClassName);
+      this._writeXObjComponent(objClassName);
+      this._writeXEditingConfig(objClassName, humanFriendlyName);
+      this._writeXObjClass(objClassName);
     });
   }
 
@@ -54,16 +36,6 @@ module.exports = class extends Generator {
     );
   }
 
-  _writeYEditingConfig(objClassName) {
-    this.fs.copyTpl(
-      this.templatePath("YEditingConfig.js.ejs"),
-      this.destinationPath(
-        `src/Objs/${objClassName}/${objClassName}EditingConfig.js`
-      ),
-      { objClassName }
-    );
-  }
-
   _writeXObjClass(objClassName) {
     this.fs.copyTpl(
       this.templatePath("XObjClass.js.ejs"),
@@ -74,13 +46,4 @@ module.exports = class extends Generator {
     );
   }
 
-  _writeYObjClass(objClassName) {
-    this.fs.copyTpl(
-      this.templatePath("YObjClass.js.ejs"),
-      this.destinationPath(
-        `src/Objs/${objClassName}/${objClassName}ObjClass.js`
-      ),
-      { objClassName }
-    );
-  }
 };

17384c089ad77ce3e0b2deb8a91791176a43ec66
Kamila Skonieczna <kamila.skonieczna@infopark.de>
2018-09-27 16:28:29 +0200

    Fixed message

--- generator-scrivito/app/index.js
+++ generator-scrivito/app/index.js
@@ -3,14 +3,14 @@ const Generator = require("yeoman-generator");
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
-    this.log("Welcome to the Scrivito obj/widget generator!");
+    this.log("Welcome to the Scrivito Obj/Widget generator");
   }
 
   start() {
     this.prompt({
       type: "list",
       name: "type",
-      message: "Welcome to the Scrivito Obj/Widget generator",
+      message: "What do you want to generate?",
       choices: ["Generate a new Obj", "Generate a new Widget"],
     }).then(answers => {
       if (answers.type === "Generate a new Widget") {

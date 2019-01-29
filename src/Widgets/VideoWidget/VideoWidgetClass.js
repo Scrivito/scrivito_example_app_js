f02b2dab1bf12962b7fad57ecad4b48992dc337f
Alexander Pepper <alexander.pepper@infopark.de>
2019-01-29 12:19:34 +0100

    Limit VideoWidget sources to Video and Download.

--- src/Widgets/VideoWidget/VideoWidgetClass.js
+++ src/Widgets/VideoWidget/VideoWidgetClass.js
@@ -2,7 +2,7 @@ import * as Scrivito from "scrivito";
 
 const VideoWidget = Scrivito.provideWidgetClass("VideoWidget", {
   attributes: {
-    source: "reference",
+    source: ["reference", { only: ["Video", "Download"] }],
     poster: ["reference", { only: ["Image"] }],
   },
 });

5a7f735b4f2edc06ea8870af0d48e6703a33572a
David Császár <dcsaszar@users.noreply.github.com>
2019-01-31 09:02:13 +0000

    Extract licence comments from bundle
    Saves 1.24 k, ~1%

--- webpack.config.js
+++ webpack.config.js
@@ -109,6 +109,7 @@ function webpackConfig(env = {}) {
       minimizer: [
         new TerserPlugin({
           cache: true,
+          extractComments: true,
           parallel: true,
           terserOptions: { ecma: 5 },
         }),

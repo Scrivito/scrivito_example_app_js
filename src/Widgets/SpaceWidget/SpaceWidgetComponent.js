01494e225a64b1a34e588a782a4932ab3f86d30d
Alexander Pepper <alexander.pepper@infopark.de>
2019-02-27 10:56:35 +0100

    Update src/Widgets/SpaceWidget/SpaceWidgetComponent.js
    Co-Authored-By: Agsiegert <agsiegert@hotmail.com>

--- src/Widgets/SpaceWidget/SpaceWidgetComponent.js
+++ src/Widgets/SpaceWidget/SpaceWidgetComponent.js
@@ -2,6 +2,6 @@ import * as React from "react";
 import * as Scrivito from "scrivito";
 
 Scrivito.provideComponent("SpaceWidget", ({ widget }) => {
-  const size = widget.get("size") || 1;
+  const size = widget.get("size") || 5;
   return <div style={{height: `${size}em`}} />;
 });

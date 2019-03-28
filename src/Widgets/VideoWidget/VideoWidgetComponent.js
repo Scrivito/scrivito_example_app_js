e36ca346e9b212e84d1442df1b807d880fe37953
Aquariusstar21 <aquariusstar21@gmail.com>
2019-03-28 16:37:46 +0900

    update video player widget

--- src/Widgets/VideoWidget/VideoWidgetComponent.js
+++ src/Widgets/VideoWidget/VideoWidgetComponent.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as Scrivito from "scrivito";
 import urlFromBinary from "../../utils/urlFromBinary";
 import videoPlaceholder from "./videoPlaceholder";
+import PlayIcon from "../../assets/images/play_icon.svg";
 
 Scrivito.provideComponent("VideoWidget", ({ widget }) => {
   const videoUrl = urlFromBinary(widget.get("source"));
@@ -18,16 +19,40 @@ Scrivito.provideComponent("VideoWidget", ({ widget }) => {
     style = videoPlaceholder;
   }
 
+  const playVideo = () => {
+    const elem = document.getElementById("video_player");
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      elem.msRequestFullscreen();
+    }
+    elem.style.display = "block";
+    elem.play();
+  };
+
   return (
-    <Scrivito.ContentTag
-      tag="video"
-      src={src}
-      content={widget}
-      attribute="source"
-      poster={posterUrl}
-      controls
-      width="100%"
-      style={style}
-    />
+    <div className="video-widget">
+      <div className="poster">
+        <img src={posterUrl} />
+        <button title="Play" onClick={playVideo}>
+          <img src={PlayIcon} />
+        </button>
+      </div>
+      <Scrivito.ContentTag
+        id="video_player"
+        tag="video"
+        src={src}
+        content={widget}
+        attribute="source"
+        poster={posterUrl}
+        controls
+        width="100%"
+        style={style}
+      />
+    </div>
   );
 });

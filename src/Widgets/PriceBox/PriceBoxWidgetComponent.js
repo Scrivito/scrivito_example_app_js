7b3150dd8ba66de7f15ba8fae875b3e98c81229e
DJ <smartprogrammer88@gmail.com>
2019-04-15 02:03:45 -0700

    priser - plan box

--- src/Widgets/PriceBox/PriceBoxWidgetComponent.js
+++ src/Widgets/PriceBox/PriceBoxWidgetComponent.js
@@ -12,75 +12,56 @@ Scrivito.provideComponent("PriceBoxWidget", ({ widget }) => {
   return (
     <div className="price-box">
       <div className="main-box">
-        <div className="fs-11 fw-600 lh-13-px">SPOT PRIS</div>
+        <div className="fs-11 fw-600 lh-13-px">SPOTPRIS</div>
         <div className="fs-24 ff-rubik-light lh-28-px midnight-text m-t-20">
-          Strøm til indkøspris
+          Prisen på strøm lige nu
         </div>
         <div className="m-t-40 divider-h m-b-40" />
-        <div className="fs-15 fw-600 lh-18-px">
-          Pris i øre/kWh
-          <br />
-          øst og vest for Storebælt
-        </div>
-        <div className="d-flex p-x-20 p-y-30 align-items-center">
+        <div className="d-flex p-x-20 align-items-center">
           <div>
-            <div className="fs-15 fw-600 lh-18-px">Vest</div>
+            <div className="fs-15 fw-600 lh-18-px">Vestdanmark</div>
             <div className="fs-15 ff-rubik-light lh-18-px m-t-20">
-              Seneste kendte pris pr. kWh
+              Pris i øre pr. kWh
             </div>
-            <div className="fs-15 lh-18-px primary-text m-t-10">
+            <div className="fs-16 ff-rubik-light lh-18-px primary-text m-t-10">
               {westLatestPrice}
             </div>
-            <div className="fs-15 ff-rubik-light lh-18-px m-t-20">
-              Samlet elpris inkl. transport og afgifter - uden abonnement
+            <div className="fs-15 ff-rubik-light lh-20-px m-t-20">
+              Samlet elpris pr. måned inkl. afgifter - uden abonnement
             </div>
-            <div className="fs-15 lh-18-px primary-text m-t-10">
+            <div className="fs-16 ff-rubik-light lh-18-px primary-text m-t-10">
               {westTotalPrice}
             </div>
             <div className="fs-12 ff-rubik-light lh-18-px m-t-10">
-              Samlet elpris inkl. transport og afgifter - uden abonnement
+              For en gennemsnitlig husstand i Aarhus
             </div>
           </div>
           <div className="divider-v" />
           <div>
-            <div className="fs-15 fw-600 lh-18-px">Vest</div>
+            <div className="fs-15 fw-600 lh-18-px">Østdanmark</div>
             <div className="fs-15 ff-rubik-light lh-18-px m-t-20">
-              Seneste kendte pris pr. kWh
+              Pris i øre pr. kWh
             </div>
-            <div className="fs-15 lh-18-px primary-text m-t-10">
+            <div className="fs-16 ff-rubik-light lh-18-px primary-text m-t-10">
               {eastLatestPrice}
             </div>
-            <div className="fs-15 ff-rubik-light lh-18-px m-t-20">
-              Samlet elpris inkl. transport og afgifter - uden abonnement
+            <div className="fs-15 ff-rubik-light lh-20-px m-t-20">
+              Samlet elpris pr. måned inkl. afgifter - uden abonnement
             </div>
-            <div className="fs-15 lh-18-px primary-text m-t-10">
+            <div className="fs-16 ff-rubik-light lh-18-px primary-text m-t-10">
               {eastTotalPrice}
             </div>
             <div className="fs-12 ff-rubik-light lh-18-px m-t-10">
-              Samlet elpris inkl. transport og afgifter - uden abonnement
+              For en gennemsnitlig husstand i Aarhus
             </div>
           </div>
         </div>
-        <div className="fs-15 fw-600 lh-20-px m-b-15">Barry abonnement</div>
+        <div className="fs-15 fw-600 lh-20-px m-t-30 m-b-15">Barry abonnement</div>
         <div className="fs-16 ff-rubik-light lh-20-px primary-text">
           <span className="fs-24 ff-rubik-light fw-300">
             {barrySubscription}
           </span>{" "}
           kr./md.
-        </div>
-      </div>
-      <div className="second-box">
-        <div className="d-flex align-items-center ff-rubik-light">
-          <div className="check-box">
-            <img src={CheckMark} />
-          </div>
-          Din pris er den samme som vores indkøbspris, og skifter time for time.
-        </div>
-        <div className="d-flex align-items-center ff-rubik-light">
-          <div className="check-box">
-            <img src={CheckMark} />
-          </div>
-          Ingen binding. Du kan slå op med Barry når du vil
         </div>
       </div>
     </div>

6fb74a8b00584fcdd5235a36c8b2511c7358d433
DJ <smartprogrammer88@gmail.com>
2019-04-30 04:33:41 -0400

    about team widget responsive

--- src/Widgets/AboutTeamWidget/AboutTeamWidgetComponent.js
+++ src/Widgets/AboutTeamWidget/AboutTeamWidgetComponent.js
@@ -66,6 +66,35 @@ const sliderSettings = {
   slidesToShow: 4,
   slidesToScroll: 1,
   speed: 500,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
 };
 
 const Addmember = ({ widget }) => {

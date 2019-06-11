b2b3cb96d5767e69fdef427a29467eb42f67d680
Alexander Pepper <alexander.pepper@infopark.de>
2019-06-11 13:32:42 +0200

    Fixed typo in RedirectToUi

--- src/Objs/RedirectToUi/RedirectToUiEditingConfig.js
+++ src/Objs/RedirectToUi/RedirectToUiEditingConfig.js
@@ -2,7 +2,7 @@ import * as Scrivito from "scrivito";
 import redirectObjIcon from "../../assets/images/redirect_obj.svg";
 
 Scrivito.provideEditingConfig("RedirectToUi", {
-  title: "Redirect To Ui",
+  title: "Redirect To UI",
   thumbnail: redirectObjIcon,
   hideInSelectionDialogs: true,
 });

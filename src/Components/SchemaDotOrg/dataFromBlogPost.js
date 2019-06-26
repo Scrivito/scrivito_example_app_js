1e3dcb84a7cc6ee8028ecf3c5028089ed20ba3a7
Alexander Pepper <alexander.pepper@infopark.de>
2019-06-26 11:32:14 +0200

    Simplified "dataFromBlogPost".

--- src/Components/SchemaDotOrg/dataFromBlogPost.js
+++ src/Components/SchemaDotOrg/dataFromBlogPost.js
@@ -5,19 +5,14 @@ import urlFromBinary from "../../utils/urlFromBinary";
 import { textExtractFromObj } from "../../utils/textExtract";
 
 function dataFromBlogPost(blogPost) {
-  let author;
-  const body = textExtractFromObj(blogPost);
-
-  if (blogPost.get("author")) {
-    author = dataFromAuthor(blogPost.get("author"));
-  }
-
   return {
     "@context": "http://schema.org",
     "@type": "BlogPosting",
-    author,
+    author: blogPost.get("author")
+      ? dataFromAuthor(blogPost.get("author"))
+      : undefined,
     datePublished: formatDate(blogPost.get("publishedAt"), "yyyy-mm-dd"),
-    description: truncate(body, { length: 300 }),
+    description: truncate(textExtractFromObj(blogPost), { length: 300 }),
     headline: blogPost.get("title"),
     image: urlFromBinary(blogPost.get("titleImage")),
     keywords: blogPost.get("tags").join(", "),

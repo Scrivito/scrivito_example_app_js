caa4bb7f0e08cd60a843e1b3c7dfd1928e916db0
Christoph Kozielski <christoph.kozielski@infopark.de>
2020-01-23 09:14:17 +0100

    Creating higher level object containing the attributes.

--- src/Objs/_defaultPageEditingConfig.js
+++ src/Objs/_defaultPageEditingConfig.js
@@ -1,6 +1,63 @@
 import HeadlineWidget from "../Widgets/HeadlineWidget/HeadlineWidgetClass";
 import SectionWidget from "../Widgets/SectionWidget/SectionWidgetClass";
 
+export const defaultPage = {
+  editingConfigAttributes: {
+    title: {
+      title: "Title",
+      description: "Limit to 55 characters.",
+    },
+    navigationBackgroundImage: {
+      title: "Header background",
+      description: "The background image/video of the header.",
+    },
+    navigationBackgroundImageGradient: {
+      title: "Use gradient for header image?",
+      description: "Only applies if a header image is selected. Default: No",
+      values: [
+        { value: "yes", title: "Yes" },
+        { value: "no", title: "No" },
+      ],
+    },
+    navigationHeight: {
+      title: "Header height",
+      description: "Default: Small",
+      values: [
+        { value: "small", title: "Small" },
+        { value: "medium-height", title: "Medium" },
+        { value: "full-height", title: "Fullscreen" },
+      ],
+    },
+  },
+  initialContent: {
+    body: [
+      new SectionWidget({
+        content: [new HeadlineWidget({ style: "h1" })],
+      }),
+    ],
+    navigationHeight: "small",
+    navigationBackgroundImageGradient: "no",
+  },
+  properties: [
+    "title",
+    "navigationHeight",
+    "navigationBackgroundImage",
+    "navigationBackgroundImageGradient",
+  ],
+  TitleValidation: [
+    "title",
+
+    title => {
+      if (title.length === 0) {
+        return {
+          message: "The title should be set.",
+          severity: "warning",
+        };
+      }
+    },
+  ],
+};
+
 export const defaultPageEditingConfigAttributes = {
   title: {
     title: "Title",
--- src/Objs/_metadataEditingConfig.js
+++ src/Objs/_metadataEditingConfig.js
@@ -1,3 +1,75 @@
+export const metadata = {
+  editingConfigAttributes: {
+    metaDataDescription: {
+      title: "Page description",
+      description: "Limit to 175, ideally 150 characters.",
+    },
+    robotsIndex: {
+      title: "Should this page be indexed?",
+      description: "If not, search engines will ignore this page. Default: Yes",
+      values: [
+        { value: "yes", title: "Yes" },
+        { value: "no", title: "No" },
+      ],
+    },
+  },
+  initialContent: {
+    robotsIndex: "yes",
+  },
+  propertiesGroup: {
+    title: "Metadata",
+    properties: ["metaDataDescription", "robotsIndex"],
+  },
+};
+
+export const socialCards = {
+  propertiesGroup: {
+    title: "Social cards",
+    component: "SocialCardsTab",
+    properties: ["tcCreator", "tcDescription", "ogDescription"],
+  },
+  validations: [
+    [
+      "tcCreator",
+
+      tcCreator => {
+        if (tcCreator && tcCreator.charAt(0) !== "@") {
+          return {
+            message: "The creator should start with @.",
+            severity: "warning",
+          };
+        }
+      },
+    ],
+    [
+      "tcDescription",
+
+      tcDescription => {
+        if (tcDescription && tcDescription.length > 200) {
+          return {
+            message:
+              "The Twitter description should not exceed 200 characters.",
+            severity: "warning",
+          };
+        }
+      },
+    ],
+    [
+      "ogDescription",
+
+      ogDescription => {
+        if (ogDescription && ogDescription.length > 300) {
+          return {
+            message:
+              "The Facebook description should not exceed 300 characters.",
+            severity: "warning",
+          };
+        }
+      },
+    ],
+  ],
+};
+
 export const metadataEditingConfigAttributes = {
   metaDataDescription: {
     title: "Page description",

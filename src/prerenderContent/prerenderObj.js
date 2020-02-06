f3cecbe526229906e5b8c7048751e6b12b6a9637
Alexander Pepper <alexander.pepper@infopark.de>
2020-02-06 14:52:50 +0100

    [prerender] Ask the "asset-manifest.json" file for css paths.

--- src/prerenderContent/prerenderObj.js
+++ src/prerenderContent/prerenderObj.js
@@ -43,7 +43,7 @@ export default async function prerenderObj(obj) {
   return [
     {
       filename: filenameFromUrl(objUrl),
-      content: generateHtml({
+      content: await generateHtml({
         objId,
         htmlAttributes,
         headContent,

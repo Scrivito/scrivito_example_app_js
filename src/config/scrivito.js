f6fc6dea7c0e81a3ff2e6070ffa757b0c7d98f96
Tilo Prütz <tilo@pruetz.net>
2020-02-19 15:49:51 +0100

    make Scrivito endpoint configurable via env like tenant or origin

--- src/config/scrivito.js
+++ src/config/scrivito.js
@@ -6,4 +6,8 @@ if (process.env.SCRIVITO_ORIGIN) {
   config.origin = process.env.SCRIVITO_ORIGIN;
 }
 
+if (process.env.SCRIVITO_ENDPOINT) {
+  config.endpoint = process.env.SCRIVITO_ENDPOINT;
+}
+
 Scrivito.configure(config);

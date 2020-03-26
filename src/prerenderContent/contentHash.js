4835030d68c0bfaf461762c4f0bcb23c1dcbbd83
Alexander Pepper <alexander.pepper@infopark.de>
2020-03-26 12:51:59 +0100

    [prerender] contentHash: md5 => md4.
    Webpack uses md4 for all hashes by default (see [1] and [2])
    
    [1] https://github.com/webpack/webpack/pull/6549
    [2] https://github.com/webpack/loader-utils/issues/114

--- src/prerenderContent/contentHash.js
+++ src/prerenderContent/contentHash.js
@@ -1,6 +1,6 @@
-import { hash as md5 } from "spark-md5";
+import md4 from "js-md4";
 
-/** Generates a 20 long hex value, based the md5 of the given string */
+/** Generates a 20 long hex value, based the md4 of the given string */
 export default async function contentHash(input) {
-  return md5(input).substr(0, 20);
+  return md4(input).substr(0, 20);
 }

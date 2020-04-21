e969d22f9c1c7d38445df0b9037dfe70d46e6ffe
Markus Waas <mail@markuswaas.de>
2020-04-22 09:15:25 +1300

    Remove incorrect space in blog schema context url

--- src/Components/SchemaDotOrg/dataFromBlog.js
+++ src/Components/SchemaDotOrg/dataFromBlog.js
@@ -3,7 +3,7 @@ import isVideoObj from "../../utils/isVideoObj";
 
 function dataFromBlog(blog) {
   const schema = {
-    "@context": "http://schema.org ",
+    "@context": "http://schema.org",
     "@type": "Blog",
     headline: blog.get("title"),
   };

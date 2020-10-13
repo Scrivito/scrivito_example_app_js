c7a2a163cd0f032d0b3f60047ca8964ca4544c97
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2020-10-13 14:28:32 +0200

    "Introducing cookieConsentManagement" which replaces "cookieConsentGiven"

---
+++ src/utils/cookieConsentManagement.js
@@ -0,0 +1,58 @@
+import * as Scrivito from "scrivito";
+
+let resolveCookie;
+let resolveCookieConsentCalledBefore = false;
+
+const cookiePromise = new Promise((resolve) => {
+  resolveCookie = resolve;
+});
+
+export function resolveCookieConsent() {
+  if (resolveCookieConsentCalledBefore) {
+    return;
+  }
+
+  resolveCookieConsentCalledBefore = true;
+
+  resolveCookie();
+}
+
+export function cookieConsentGiven() {
+  if (getCookieConsent() === "accepted") {
+    resolveCookieConsent();
+  }
+
+  return Scrivito.finishLoading().then(() => cookiePromise);
+}
+
+export function cookieConsentUrl() {
+  const root = Scrivito.Obj.root();
+
+  if (!root) {
+    return null;
+  }
+
+  const cookieConsentLink = root.get("cookieConsentLink");
+
+  if (!cookieConsentLink) {
+    return null;
+  }
+
+  return {
+    url: Scrivito.urlFor(cookieConsentLink),
+    title: cookieConsentLink.title() || "Learn more »",
+  };
+}
+
+export function getCookieConsent() {
+  return localStorage.getItem("CookieConsent") || "undecided";
+}
+
+export function acceptCookieConsent() {
+  localStorage.setItem("CookieConsent", "accepted");
+
+  resolveCookieConsent();
+}
+export function declineCookieConsent() {
+  localStorage.setItem("CookieConsent", "declined");
+}

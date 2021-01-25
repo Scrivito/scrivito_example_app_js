0b6053b6c713270e4efb8ce28cae9d6af3425917
Piotr Pietruszka <piotr.pietruszka@infopark.de>
2021-01-25 10:50:53 +0100

    fix problem with hot module reloading

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,7 @@ function webpackConfig(env = {}) {
     mode: isProduction ? "production" : "development",
     context: path.join(__dirname, "src"),
     entry: generateEntry({ isPrerendering }),
-    target: ["web", "es5"],
+    target: isProduction ? ["web", "es5"] : "web",
     module: {
       rules: [
         {

ae4d807c9e00efd01e3e4add0b54039d9e0c0e70
Alexander Pepper <alexander.pepper@infopark.de>
2021-03-15 09:39:18 +0100

    Limit VideoWidget to only allow "Video" obj classes as sources.

--- src/Widgets/VideoWidget/VideoWidgetClass.js
+++ src/Widgets/VideoWidget/VideoWidgetClass.js
@@ -2,7 +2,7 @@ import * as Scrivito from "scrivito";
 
 const VideoWidget = Scrivito.provideWidgetClass("VideoWidget", {
   attributes: {
-    source: ["reference", { only: ["Video", "Download"] }],
+    source: ["reference", { only: ["Video"] }],
     poster: ["reference", { only: ["Image"] }],
   },
 });

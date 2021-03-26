a71175a6351629847cc95086fd9027e8ea72ba5e
David Császár <dcsaszar@users.noreply.github.com>
2021-03-26 16:55:51 +0100

    Set a fallback screen width for SSR
    The value used is based on (and thus optimized for) the Lighthouse mobile emulation:
    https://github.com/GoogleChrome/lighthouse/blob/777bf1147fd0f6aca16ffefde1350bf6297476d4/lighthouse-core/config/constants.js#L58-L63

--- src/utils/fullScreenWidthPixels.js
+++ src/utils/fullScreenWidthPixels.js
@@ -1,6 +1,12 @@
 import devicePixelRatio from "./devicePixelRatio";
 
+const SSR_FALLBACK_WIDTH_PX = 945;
+
 function fullScreenWidthPixels() {
+  if (typeof window === "undefined") {
+    return SSR_FALLBACK_WIDTH_PX;
+  }
+
   const screenWidth = window.screen.width;
 
   return screenWidth * devicePixelRatio();

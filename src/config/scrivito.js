2a96dc602a967bd9c9f805291769e39a574dfc9f
David Császár <dcsaszar@users.noreply.github.com>
2021-03-26 17:02:49 +0100

    Make configureScrivito side-effect free
    This is general best practice, but also handy when running the code under Node.js in conjunction with `dotenv`.

--- src/config/scrivito.js
+++ src/config/scrivito.js
@@ -1,16 +1,16 @@
 import * as Scrivito from "scrivito";
 
-const config = { tenant: process.env.SCRIVITO_TENANT, adoptUi: true };
-
-if (process.env.SCRIVITO_ORIGIN) {
-  config.origin = process.env.SCRIVITO_ORIGIN;
-}
-
-if (process.env.SCRIVITO_ENDPOINT) {
-  config.endpoint = process.env.SCRIVITO_ENDPOINT;
-}
-
 export function configureScrivito(options) {
+  const config = { tenant: process.env.SCRIVITO_TENANT, adoptUi: true };
+
+  if (process.env.SCRIVITO_ORIGIN) {
+    config.origin = process.env.SCRIVITO_ORIGIN;
+  }
+
+  if (process.env.SCRIVITO_ENDPOINT) {
+    config.endpoint = process.env.SCRIVITO_ENDPOINT;
+  }
+
   const priority = options?.priority;
   Scrivito.configure(priority ? { ...config, priority } : config);
 }

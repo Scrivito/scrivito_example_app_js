8f57928fe9ffe5a5a8fdcfeea8900ab7fc2620f1
David Császár <dcsaszar@users.noreply.github.com>
2021-04-20 06:13:25 +0200

    Make preload dump spec compliant
    * Escape according to https://262.ecma-international.org/5.1/#sec-7.8.4

--- src/prerenderContent/generatePreloadDump.js
+++ src/prerenderContent/generatePreloadDump.js
@@ -1,3 +1,13 @@
 export default function generatePreloadDump(preloadDump) {
-  return `window.preloadDump = ${JSON.stringify(preloadDump)};`;
+  return `window.preloadDump = ${stringLiteral(preloadDump)};`;
+}
+
+/**
+ * JSON may contain line separators, which are invalid in string literals in older browsers.
+ * See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify#issue_with_plain_json.stringify_for_use_as_javascript
+ * */
+function stringLiteral(string) {
+  return JSON.stringify(string)
+    .replace(/\u2028/g, "\\u2028")
+    .replace(/\u2029/g, "\\u2029");
 }

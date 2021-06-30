1f3334bded15c56e1fac7190e54f8637bd2d23f1
Alexander Pepper <alexander.pepper@infopark.de>
2021-06-30 14:02:58 +0200

    IconSearchResults: Converted to react hooks
    Diff best viewed without whitespace changes

--- src/Components/ScrivitoExtensions/IconEditorTab/IconSearchResults.js
+++ src/Components/ScrivitoExtensions/IconEditorTab/IconSearchResults.js
@@ -3,48 +3,43 @@ import Fuse from "fuse.js";
 import fontAwesomeIcons from "./fontAwesomeIcons";
 import SingleIcon from "./SingleIcon";
 
-class IconSearchResults extends React.Component {
-  constructor(props) {
-    super(props);
-
+function IconSearchResults({ searchValue, setWidgetIcon, currentIcon }) {
+  const fuse = React.useMemo(() => {
     const fuseOptions = {
       threshold: 0.2,
       keys: ["name", "id", "filter", "aliases"],
     };
-    this.fuse = new Fuse(fontAwesomeIcons, fuseOptions);
+
+    return new Fuse(fontAwesomeIcons, fuseOptions);
+  }, []);
+
+  if (!searchValue.length) {
+    return null;
   }
 
-  render() {
-    const { searchValue, setWidgetIcon, currentIcon } = this.props;
+  const results = fuse.search(searchValue);
 
-    if (!searchValue.length) {
-      return null;
-    }
-
-    const results = this.fuse.search(searchValue);
-
-    return (
-      <div id="search-results">
-        <div className="scrivito_detail_label">
-          <span>{`Search for '${searchValue}'`}</span>
-        </div>
-        <div className="row">
-          {results.map((result, index) => {
-            const icon = result.item;
-
-            return (
-              <SingleIcon
-                key={`${icon.id}${index}`}
-                icon={icon}
-                currentIcon={currentIcon}
-                setWidgetIcon={setWidgetIcon}
-              />
-            );
-          })}
-        </div>
+  return (
+    <div id="search-results">
+      <div className="scrivito_detail_label">
+        <span>{`Search for '${searchValue}'`}</span>
       </div>
-    );
-  }
+      <div className="row">
+        {results.map((result, index) => {
+          const icon = result.item;
+
+          return (
+            <SingleIcon
+              key={`${icon.id}${index}`}
+              icon={icon}
+              currentIcon={currentIcon}
+              setWidgetIcon={setWidgetIcon}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default IconSearchResults;

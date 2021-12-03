8e7a07f879b205d33c21accf8dffa992a237e772
Alexander Pepper <alexander.pepper@justrelate.com>
2021-12-03 14:42:32 +0100

    FormContainerWidget pre-generates a formId.
    Generate formId via with pseudo-random values.
    
    Do it it this way no external library (like blueimp-md5 to md5 obj and widget id) is needed.

--- src/Widgets/FormContainerWidget/FormContainerWidgetClass.js
+++ src/Widgets/FormContainerWidget/FormContainerWidgetClass.js
@@ -3,6 +3,7 @@ import * as Scrivito from "scrivito";
 const FormContainerWidget = Scrivito.provideWidgetClass("FormContainerWidget", {
   attributes: {
     content: "widgetlist",
+    formId: "string",
   },
   extractTextAttributes: ["content"],
 });
--- src/Widgets/FormContainerWidget/FormContainerWidgetComponent.js
+++ src/Widgets/FormContainerWidget/FormContainerWidgetComponent.js
@@ -6,6 +6,7 @@ import "./FormContainerWidget.scss";
 Scrivito.provideComponent("FormContainerWidget", ({ widget }) => (
   <div className="form-container-widget">
     <form method="post">
+      <input type="hidden" name="form_id" value={widget.get("formId")} />
       <Scrivito.ContentTag content={widget} attribute="content" />
     </form>
   </div>

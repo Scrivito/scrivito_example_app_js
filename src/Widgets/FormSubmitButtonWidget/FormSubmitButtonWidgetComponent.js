29199757651d68cfb01dcd9f20e68b8ab8d84b88
Alexander Pepper <alexander.pepper@justrelate.com>
2021-12-07 13:50:30 +0100

    Refactored FormSubmitButtonWidget

--- src/Widgets/FormSubmitButtonWidget/FormSubmitButtonWidgetComponent.js
+++ src/Widgets/FormSubmitButtonWidget/FormSubmitButtonWidgetComponent.js
@@ -1,22 +1,19 @@
 import * as React from "react";
 import * as Scrivito from "scrivito";
 
-Scrivito.provideComponent("FormSubmitButtonWidget", ({ widget }) => {
-  if (["center", "right"].includes(widget.get("alignment"))) {
-    return (
-      <div className={`text-${widget.get("alignment")}`}>
-        <Button buttonText={widget.get("buttonText")} />
-      </div>
-    );
-  }
-
-  return <Button buttonText={widget.get("buttonText")} />;
-});
-
-function Button({ buttonText }) {
-  return (
+Scrivito.provideComponent("FormSubmitButtonWidget", ({ widget }) => (
+  <WrapIfClassName
+    className={
+      ["center", "right"].includes(widget.get("alignment")) &&
+      `text-${widget.get("alignment")}`
+    }
+  >
     <button className="btn btn-primary" type="submit">
-      {buttonText}
+      {widget.get("buttonText")}
     </button>
-  );
+  </WrapIfClassName>
+));
+
+function WrapIfClassName({ className, children }) {
+  return className ? <div className={className}>{children}</div> : children;
 }

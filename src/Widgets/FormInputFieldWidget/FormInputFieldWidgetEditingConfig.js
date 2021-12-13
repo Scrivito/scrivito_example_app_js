1149aeb8a5c1888970be22a49baf02667f52e4e6
Alexander Pepper <alexander.pepper@justrelate.com>
2021-12-13 13:56:00 +0100

    FormInputFieldWidget: Check for missing or invalid values via validations.

--- src/Widgets/FormInputFieldWidget/FormInputFieldWidgetEditingConfig.js
+++ src/Widgets/FormInputFieldWidget/FormInputFieldWidgetEditingConfig.js
@@ -40,4 +40,38 @@ Scrivito.provideEditingConfig("FormInputFieldWidget", {
     "required",
     "helpText",
   ],
+  validations: [
+    [
+      "type",
+      (type) => {
+        if (!type) {
+          return "Select the input type.";
+        }
+      },
+    ],
+    [
+      "customFieldName",
+      (customFieldName, { widget }) => {
+        if (!widget.get("type")?.startsWith("custom")) {
+          return;
+        }
+
+        if (!customFieldName.startsWith("custom_")) {
+          return 'Custom field names must start with "custom_".';
+        }
+
+        if (customFieldName.length <= "custom_".length) {
+          return "Specify the custom field's name.";
+        }
+
+        if (customFieldName.match(/^[A-Za-z_][A-Za-z0-9_]*$/) === null) {
+          return 'Custom field names may consist of the following characters: "a-z", "A-Z", "0-9", "_".';
+        }
+
+        if (customFieldName.length > 50) {
+          return "Custom field names may be up to 50 characters long.";
+        }
+      },
+    ],
+  ],
 });

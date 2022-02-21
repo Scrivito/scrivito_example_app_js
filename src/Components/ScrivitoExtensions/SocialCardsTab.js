6794ed309529c8f92048380bd5df8f6f83754e1a
Alexander Pepper <alexander.pepper@justrelate.com>
2022-02-21 10:11:18 +0100

    EditAttribute.js => ContentProperty.js

--- src/Components/ScrivitoExtensions/SocialCardsTab.js
+++ src/Components/ScrivitoExtensions/SocialCardsTab.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as Scrivito from "scrivito";
-import EditAttribute from "./EditAttribute";
+import EditAttribute from "./ContentProperty";
 import getMetadata from "../../utils/getMetadata";
 
 const SocialCardsTab = Scrivito.connect(({ obj }) => {

80323eb1dff6d52aea2b501654dd3d4395655941
Alexander Pepper <alexander.pepper@justrelate.com>
2022-02-22 13:33:30 +0100

    Fix "Uncaught TypeError: candidate.container is not a function"

--- src/Widgets/FormContainerWidget/utils/getFormContainer.js
+++ src/Widgets/FormContainerWidget/utils/getFormContainer.js
@@ -3,12 +3,12 @@ import * as Scrivito from "scrivito";
 /** Returns the first `FormContainerWidget` container it can find. `null` otherwise. */
 export function getFormContainer(childWidget) {
   let candidate = childWidget.container();
-  do {
+  while (candidate instanceof Scrivito.Widget) {
     if (candidate.objClass() === "FormContainerWidget") {
       return candidate;
     }
     candidate = candidate.container();
-  } while (candidate instanceof Scrivito.Widget);
+  }
 
   return null;
 }

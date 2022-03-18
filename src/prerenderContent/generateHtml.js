002d2395e9b2fc5601f5a3ae4678caafdc10d5e1
Alexander Pepper <alexander.pepper@justrelate.com>
2022-03-18 11:10:56 +0100

    Removed now obsolete "X-UA-Compatible" meta tag.
    According to [1] this is no longer in use with Microsoft Edge.
    
    [1] https://docs.microsoft.com/en-us/openspecs/ie_standards/ms-iedoco/380e2488-f5eb-4457-a07a-0cb1b6e4b4b5

--- src/prerenderContent/generateHtml.js
+++ src/prerenderContent/generateHtml.js
@@ -11,7 +11,6 @@ export default async function generateHtml({
 <html ${htmlAttributes}>
   <head>
     <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta
       name="generator"

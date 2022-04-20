ffe25da32f98f845963b3312c0b47e059790f858
Alexander Pepper <alexander.pepper@justrelate.com>
2022-04-20 11:10:28 +0200

    Convert all "src/config" to new single line style.

--- src/config/scrivito.js
+++ src/config/scrivito.js
@@ -9,17 +9,13 @@ export function configureScrivito(options) {
     tenant: process.env.SCRIVITO_TENANT,
   };
 
-  if (process.env.SCRIVITO_ORIGIN) {
-    config.origin = process.env.SCRIVITO_ORIGIN;
-  }
+  if (process.env.SCRIVITO_ORIGIN) config.origin = process.env.SCRIVITO_ORIGIN;
 
   if (process.env.SCRIVITO_ENDPOINT) {
     config.endpoint = process.env.SCRIVITO_ENDPOINT;
   }
 
-  if (options?.priority) {
-    config.priority = options.priority;
-  }
+  if (options?.priority) config.priority = options.priority;
 
   Scrivito.configure(config);
 }

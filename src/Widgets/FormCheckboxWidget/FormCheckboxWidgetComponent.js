24b46326e308edfe2c1885eeef7e78c478f235fd
Alexander Pepper <alexander.pepper@justrelate.com>
2022-04-26 11:17:38 +0200

    [FormXWidget] Use newer react-bootstrap (so it works with Bootstrap 5)

--- src/Widgets/FormCheckboxWidget/FormCheckboxWidgetComponent.js
+++ src/Widgets/FormCheckboxWidget/FormCheckboxWidgetComponent.js
@@ -1,12 +1,10 @@
 import * as React from "react";
 import * as Scrivito from "scrivito";
-import { UncontrolledPopover, PopoverBody } from "reactstrap";
+import { Popover, OverlayTrigger } from "react-bootstrap";
 import { getFieldName } from "../FormContainerWidget/utils/getFieldName";
 
 Scrivito.provideComponent("FormCheckboxWidget", ({ widget }) => {
   const id = `form_checkbox_widget_${widget.id()}`;
-  const questionMarkId = `${id}_question_mark`;
-  const mandatoryId = `${id}_mandatory`;
 
   const labelOptions = {};
   if (!Scrivito.isInPlaceEditingActive()) {
@@ -31,38 +29,35 @@ Scrivito.provideComponent("FormCheckboxWidget", ({ widget }) => {
         {...labelOptions}
       />
       {widget.get("required") ? (
-        <>
-          <span className="text-mandatory help-cursor" id={mandatoryId}>
-            *
-          </span>
-          <UncontrolledPopover
-            placement="bottom"
-            target={mandatoryId}
-            trigger="legacy"
-          >
-            <PopoverBody>mandatory</PopoverBody>
-          </UncontrolledPopover>
-        </>
+        <OverlayTrigger
+          placement="bottom"
+          trigger="click"
+          overlay={
+            <Popover>
+              <Popover.Body>mandatory</Popover.Body>
+            </Popover>
+          }
+        >
+          <span className="text-mandatory help-cursor">*</span>
+        </OverlayTrigger>
       ) : null}
 
       {widget.get("helpText") ? (
-        <>
-          <i
-            className="fa fa-question-circle-o fa-1x ml-1 help-cursor"
-            id={questionMarkId}
-          ></i>
-          <UncontrolledPopover
-            placement="bottom"
-            target={questionMarkId}
-            trigger="legacy"
-          >
-            <PopoverBody>
-              <Scrivito.InPlaceEditingOff>
-                <Scrivito.ContentTag content={widget} attribute="helpText" />
-              </Scrivito.InPlaceEditingOff>
-            </PopoverBody>
-          </UncontrolledPopover>
-        </>
+        <OverlayTrigger
+          placement="bottom"
+          trigger="click"
+          overlay={
+            <Popover>
+              <Popover.Body>
+                <Scrivito.InPlaceEditingOff>
+                  <Scrivito.ContentTag content={widget} attribute="helpText" />
+                </Scrivito.InPlaceEditingOff>
+              </Popover.Body>
+            </Popover>
+          }
+        >
+          <i className="fa fa-question-circle-o fa-1x ml-1 help-cursor"></i>
+        </OverlayTrigger>
       ) : null}
     </div>
   );
--- src/Widgets/FormInputFieldWidget/FormInputFieldWidgetComponent.js
+++ src/Widgets/FormInputFieldWidget/FormInputFieldWidgetComponent.js
@@ -1,13 +1,11 @@
 import * as React from "react";
 import * as Scrivito from "scrivito";
-import { UncontrolledPopover, PopoverBody } from "reactstrap";
+import { Popover, OverlayTrigger } from "react-bootstrap";
 import { getFieldName } from "../FormContainerWidget/utils/getFieldName";
 import { isCustomType } from "../FormContainerWidget/utils/isCustomType";
 
 Scrivito.provideComponent("FormInputFieldWidget", ({ widget }) => {
   const id = `form_text_input_widget_${widget.id()}`;
-  const questionMarkId = `${id}_question_mark`;
-  const mandatoryId = `${id}_mandatory`;
 
   const fieldName = getFieldName(widget);
   const labelOptions = {};
@@ -24,38 +22,35 @@ Scrivito.provideComponent("FormInputFieldWidget", ({ widget }) => {
         {...labelOptions}
       />
       {widget.get("required") ? (
-        <>
-          <span className="text-mandatory help-cursor" id={mandatoryId}>
-            *
-          </span>
-          <UncontrolledPopover
-            placement="bottom"
-            target={mandatoryId}
-            trigger="legacy"
-          >
-            <PopoverBody>mandatory</PopoverBody>
-          </UncontrolledPopover>
-        </>
+        <OverlayTrigger
+          placement="bottom"
+          trigger="click"
+          overlay={
+            <Popover>
+              <Popover.Body>mandatory</Popover.Body>
+            </Popover>
+          }
+        >
+          <span className="text-mandatory help-cursor">*</span>
+        </OverlayTrigger>
       ) : null}
 
       {widget.get("helpText") ? (
-        <>
-          <i
-            className="fa fa-question-circle-o fa-1x ml-1 help-cursor"
-            id={questionMarkId}
-          ></i>
-          <UncontrolledPopover
-            placement="bottom"
-            target={questionMarkId}
-            trigger="legacy"
-          >
-            <PopoverBody>
-              <Scrivito.InPlaceEditingOff>
-                <Scrivito.ContentTag content={widget} attribute="helpText" />
-              </Scrivito.InPlaceEditingOff>
-            </PopoverBody>
-          </UncontrolledPopover>
-        </>
+        <OverlayTrigger
+          placement="bottom"
+          trigger="click"
+          overlay={
+            <Popover>
+              <Popover.Body>
+                <Scrivito.InPlaceEditingOff>
+                  <Scrivito.ContentTag content={widget} attribute="helpText" />
+                </Scrivito.InPlaceEditingOff>
+              </Popover.Body>
+            </Popover>
+          }
+        >
+          <i className="fa fa-question-circle-o fa-1x ml-1 help-cursor"></i>
+        </OverlayTrigger>
       ) : null}
 
       {isCustomType(widget) && widget.get("customType") === "multi_line" ? (

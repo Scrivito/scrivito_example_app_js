838a00ed14d452d401fc0ded62e70e090c545a54
Alexander Pepper <alexander.pepper@justrelate.com>
2023-01-03 11:24:10 +0100

    isImageObj is now more robust.
    Previously isImageObj assumed, that the passed in obj is either null or Scrivito.Obj. But it can be any scrivito attribute value, e.g. a string, a binary or a widgetlist.

--- src/utils/isImageObj.js
+++ src/utils/isImageObj.js
@@ -1,3 +1,7 @@
-export function isImageObj(object) {
-  return object && object.objClass() === "Image" && object.get("blob");
+import * as Scrivito from "scrivito";
+
+export function isImageObj(obj) {
+  return (
+    obj instanceof Scrivito.Obj && obj.objClass() === "Image" && obj.get("blob")
+  );
 }
